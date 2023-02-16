5d9a921d4cc57fecb0fdeff741a1216a9eb43280
unknown <eyuptrabzonn@gmail.com>
2023-02-17 00:19:36 +0100

    Update DatabaseManager.ts

--- database/DatabaseManager.ts
+++ database/DatabaseManager.ts
@@ -8,13 +8,13 @@ export default class DatabaseManager extends DatabaseServer {
     super();
   }
 
-  public static getInstance() {
+  public static GetInstance() {
     if (this.DatabaseManager == null)
       this.DatabaseManager = new DatabaseManager();
 
     return this.DatabaseManager;
   }
-  public async query(st: string): Promise<[{}]> {
+  public async Query(st: string): Promise<[{}]> {
     return new Promise((resolve, reject) => {
       this.Connection.query(st, function (error, results, fields) {
         if (error) resolve([{ status: "error" }]);
