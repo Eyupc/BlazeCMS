b53436970c728b2ab0c364d81832d4ef4ac5d2cf
unknown <eyuptrabzonn@gmail.com>
2023-02-17 00:19:42 +0100

    Update test.ts

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -5,7 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<[{}]>
 ) {
-  let data = await DatabaseManager.getInstance().query(
+  let data = await DatabaseManager.GetInstance().Query(
     "SELECT username FROM users"
   );
   res.status(200).json(data);
