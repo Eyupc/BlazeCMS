d54e372935d8e57354609b8eb74c43e33a9d9bbb
unknown <eyuptrabzonn@gmail.com>
2023-03-02 00:02:37 +0100

    API for avatar by using username

---
+++ src/pages/api/avatar/image.ts
@@ -0,0 +1,24 @@
+import DatabaseManager from "database/DatabaseManager";
+import { setServers } from "dns";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession, useSession } from "next-auth/react";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{}>
+) {
+  let data = await DatabaseManager.GetInstance().Query(
+    `SELECT look FROM users WHERE username = '${req.query.username}'`
+  );
+  console.log(data);
+  if (!data.error) {
+    res.status(200).json({
+      status: "OK",
+      username: req.query.username,
+      look: data.data[0].look,
+    });
+  }
+  res.status(200).json({
+    status: "ERROR",
+  });
+}
