151b3d7c922e14718122aa84568c516ebdfe8940
unknown <eyuptrabzonn@gmail.com>
2023-03-02 00:01:24 +0100

    some stuff done

--- database/DatabaseManager.ts
+++ database/DatabaseManager.ts
@@ -14,17 +14,18 @@ export default class DatabaseManager extends DatabaseServer {
 
     return this.DatabaseManager;
   }
-  public async Query(st: string): Promise<[{}]> {
+  public async Query(st: string): Promise<{ error: boolean; data?: any }> {
     return new Promise((resolve, reject) => {
-      this.Connection.query(st, function (error, results, fields) {
-        if (error) resolve([{ status: "error" }]);
-        resolve(results);
+      this.Connection.query(st, function (_error, results, fields) {
+        if (_error || results.length == 0) resolve({ error: true });
+
+        resolve({ error: false, data: results });
       });
     });
   }
   public async GetUser(username: string): Promise<{
     status: string;
-    id?: string;
+    id?: number;
     username?: string;
     mail?: string;
     password?: string;
@@ -34,20 +35,21 @@ export default class DatabaseManager extends DatabaseServer {
     return new Promise((resolve, reject) => {
       this.Connection.query(
         "SELECT `id`,`username`,`password`,`mail`,`rank`,`auth_ticket` FROM `users` WHERE `username`=" +
-          `"${username}"` +
-          " LIMIT 1",
+          `"${username}"`,
         function (error, results, fields) {
-          console.log(error);
-          if (error) resolve({ status: "ERR" });
-          resolve({
-            status: "OK",
-            id: results[0].id,
-            username: results[0].username,
-            mail: results[0].mail,
-            password: results[0].password,
-            rank: results[0].rank,
-            auth_ticket: results[0].auth_ticket,
-          });
+          if (error || results.length == 0) {
+            resolve({ status: "ERR" });
+          } else {
+            resolve({
+              status: "OK",
+              id: results[0].id,
+              username: results[0].username,
+              mail: results[0].mail,
+              password: results[0].password,
+              rank: results[0].rank,
+              auth_ticket: results[0].auth_ticket,
+            });
+          }
         }
       );
     });
