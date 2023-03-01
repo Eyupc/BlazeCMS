6e625063a2588fb835a7318da2dd32cc8e1f2dc6
unknown <eyuptrabzonn@gmail.com>
2023-03-02 00:02:43 +0100

    Test API

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,10 +1,13 @@
 import DatabaseManager from "database/DatabaseManager";
+import { setServers } from "dns";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession, useSession } from "next-auth/react";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{}>
 ) {
+  let ses = await getSession({ req });
   let data = await DatabaseManager.GetInstance().GetUser("Eyup");
-  res.status(200).json(data);
+  res.status(200).json(ses!);
 }
