54513317c2c930f0d610897a6a06dd6b00e6db74
unknown <eyuptrabzonn@gmail.com>
2023-03-06 00:08:37 +0100

    cleanup

--- database/DatabaseManager.ts
+++ database/DatabaseManager.ts
@@ -2,6 +2,15 @@ import { Connection } from "mysql";
 import { Type } from "typescript";
 import DatabaseServer from "./DatabaseServer";
 
+type User = {
+  status: string;
+  id?: number;
+  username?: string;
+  mail?: string;
+  password?: string;
+  rank?: number;
+  auth_ticket?: string;
+};
 export default class DatabaseManager extends DatabaseServer {
   private static DatabaseManager: DatabaseManager | null = null;
   constructor() {
@@ -23,15 +32,7 @@ export default class DatabaseManager extends DatabaseServer {
       });
     });
   }
-  public async GetUser(username: string): Promise<{
-    status: string;
-    id?: number;
-    username?: string;
-    mail?: string;
-    password?: string;
-    rank?: number;
-    auth_ticket?: string;
-  }> {
+  public async GetUser(username: string): Promise<User> {
     return new Promise((resolve, reject) => {
       this.Connection.query(
         "SELECT `id`,`username`,`password`,`mail`,`rank`,`auth_ticket` FROM `users` WHERE `username`=" +
