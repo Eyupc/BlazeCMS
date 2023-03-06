17d5d09d29904a759bfb26cfd38d73f7674c5bc8
unknown <eyuptrabzonn@gmail.com>
2023-03-07 00:42:29 +0100

    Added GetUserCurrencies

--- database/DatabaseManager.ts
+++ database/DatabaseManager.ts
@@ -1,16 +1,8 @@
 import { Connection } from "mysql";
 import { Type } from "typescript";
 import DatabaseServer from "./DatabaseServer";
+import { User, UserCurrencies } from "./types/UserTypes";
 
-type User = {
-  status: string;
-  id?: number;
-  username?: string;
-  mail?: string;
-  password?: string;
-  rank?: number;
-  auth_ticket?: string;
-};
 export default class DatabaseManager extends DatabaseServer {
   private static DatabaseManager: DatabaseManager | null = null;
   constructor() {
@@ -32,6 +24,7 @@ export default class DatabaseManager extends DatabaseServer {
       });
     });
   }
+
   public async GetUser(username: string): Promise<User> {
     return new Promise((resolve, reject) => {
       this.Connection.query(
@@ -46,9 +39,34 @@ export default class DatabaseManager extends DatabaseServer {
               id: results[0].id,
               username: results[0].username,
               mail: results[0].mail,
+              motto: results[0].motto,
+              look: results[0].look,
               password: results[0].password,
               rank: results[0].rank,
               auth_ticket: results[0].auth_ticket,
+              ip_last: results[0].ip_last,
+              ip_reg: results[0].ip_reg,
+            });
+          }
+        }
+      );
+    });
+  }
+
+  public async GetUserCurrencies(id: number): Promise<UserCurrencies> {
+    return new Promise((resolve, reject) => {
+      this.Connection.query(
+        "SELECT u.credits AS credits,uc.type, uc.amount as AANTAL FROM `users` u INNER JOIN `users_currency` uc ON `u`.id = `uc`.user_id WHERE `u`.id = " +
+          `${id};`,
+        function (_error, results, fields) {
+          if (_error || results.length == 0) {
+            resolve({ status: "ERR" });
+          } else {
+            resolve({
+              status: "OK",
+              credits: results[0].credits,
+              duckets: results[0].AANTAL,
+              diamonds: results[1].AANTAL,
             });
           }
         }
