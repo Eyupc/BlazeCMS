351ba29b9b4c7b39ab9c6de73d9be8b013be3e2a
unknown <eyuptrabzonn@gmail.com>
2023-03-07 00:43:54 +0100

    Update next.d.ts

--- types/next.d.ts
+++ types/next.d.ts
@@ -1,16 +1,22 @@
-import type { NextComponentType, NextPageContext } from "next"
-import type { Session } from "next-auth"
-import type { Router } from "next/router"
+import type { NextComponentType, NextPageContext } from "next";
+import type { Session } from "next-auth";
+import type { Router } from "next/router";
+import { DOMAttributes } from "react";
 
 declare module "next/app" {
   type AppProps<P = Record<string, unknown>> = {
-    Component: NextComponentType<NextPageContext, any, P>
-    router: Router
-    __N_SSG?: boolean
-    __N_SSP?: boolean
+    Component: NextComponentType<NextPageContext, any, P>;
+    router: Router;
+    __N_SSG?: boolean;
+    __N_SSP?: boolean;
     pageProps: P & {
       /** Initial session passed in from `getServerSideProps` or `getInitialProps` */
-      session?: Session
-    }
+      session?: Session;
+    };
+  };
+}
+declare namespace React {
+  interface HTMLProps<T> extends HTMLAttributes, ClassAttributes<T> {
+    active?: string;
   }
 }
