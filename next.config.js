f5f6bd4e6f226ecaaafaa126b8df4409462ee737
unknown <eyuptrabzonn@gmail.com>
2023-03-10 00:36:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  env: {
-    MYSQL_HOST: "127.0.0.1",
-    MYSQL_PORT: "3306",
-    MYSQL_DATABASE: "retro",
-    MYSQL_USER: "root",
-    MYSQL_PASSWORD: "root",
-    NEXTAUTH_URL: "http://localhost:3000",
-    SECRET: "Eyup-is-dit",
-  },
 };
 
 module.exports = nextConfig;
