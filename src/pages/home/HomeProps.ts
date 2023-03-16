4c3f519099f809aaa4c4bb9d06734c43197e4f6a
unknown <eyuptrabzonn@gmail.com>
2023-03-16 23:28:41 +0100

    Create HomeProps.ts

---
+++ src/pages/home/HomeProps.ts
@@ -0,0 +1,14 @@
+export type HomeProps = {
+  user?: {
+    username: string;
+    motto: string;
+    look: string;
+    credits: number;
+    duckets: number;
+    diamonds: number;
+    achievement_score: number;
+    //last_login: string;
+    last_online: string;
+    online: boolean;
+  };
+};
