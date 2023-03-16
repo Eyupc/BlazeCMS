d3f9eb89c62d11aa50a35f3bfc3aa34805cd1da1
unknown <eyuptrabzonn@gmail.com>
2023-03-16 23:26:13 +0100

    Update UserTypes.ts

--- database/types/UserTypes.ts
+++ database/types/UserTypes.ts
@@ -1,27 +1,37 @@
+import { StatusType } from "./StatusType";
+
 export type User = {
-  status: "OK" | "ERROR";
-  id?: number;
-  username?: string;
-  look?: string;
-  mail?: string;
-  motto?: string;
-  password?: string;
-  rank?: number;
-  auth_ticket?: string;
-  ip_last?: string;
-  ip_reg?: string;
+  status: StatusType["status"];
+  data?: {
+    id: number;
+    username: string;
+    look: string;
+    mail: string;
+    motto: string;
+    rank: number;
+    last_login: EpochTimeStamp;
+    ip_register: string;
+    ip_current: string;
+    last_online: EpochTimeStamp;
+    online: boolean;
+  };
 };
 export type UserCurrencies = {
-  status: "OK" | "ERROR";
-  credits?: number;
-  duckets?: number;
-  diamonds?: number;
+  status: StatusType["status"];
+  data?: {
+    credits: number;
+    duckets: number;
+    diamonds: number;
+  };
 };
 export type UserSettings = {
-  status: "OK" | "ERROR";
-  user_id?: number;
-  achievement_score?: number;
-  respects_given?: number;
-  respects_received?: number;
-  can_trade?: boolean;
+  status: StatusType["status"];
+  data?: {
+    user_id: number;
+    achievement_score: number;
+    respects_given: number;
+    respects_received: number;
+    can_trade: boolean;
+    block_friendrequest: boolean;
+  };
 };
