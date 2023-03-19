def9545950ad266798639c721c79ea78cd846cd3
unknown <eyuptrabzonn@gmail.com>
2023-03-19 23:58:37 +0100

    ChangeAvatar function

---
+++ src/app/Index/methods/ChangeAvatar.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+
+export const ChangeAvatar = async (username: string) => {
+  return await axios({
+    method: "GET",
+    url: `/api/avatar/${username}`,
+    responseType: "json",
+  })
+    .then(function (response) {
+      if (response.data.status) {
+        return response.data.look;
+      } else {
+        return "";
+      }
+    })
+    .catch((err) => {
+      return "";
+    });
+};
