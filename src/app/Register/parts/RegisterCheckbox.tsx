906433b89ae31e8a12db91a2f01f9cfae1f08dec
unknown <eyuptrabzonn@gmail.com>
2023-03-20 22:33:45 +0100

    Create RegisterCheckbox.tsx

---
+++ src/app/Register/parts/RegisterCheckbox.tsx
@@ -0,0 +1,24 @@
+import { memo } from "react";
+import { IRegisterCheckbox } from "../interfaces/IRegisterCheckbox";
+
+export const RegisterCheckbox = memo(({ description }: IRegisterCheckbox) => {
+  return (
+    <fieldset className="fieldset">
+      <div className="field">
+        <label className="form-label-checkbox">
+          <input
+            id="terms-of-service"
+            type="checkbox"
+            className="form-checkbox"
+            required
+          />
+          <span>
+            <strong>
+              <u>{description}</u>
+            </strong>
+          </span>
+        </label>
+      </div>
+    </fieldset>
+  );
+});
