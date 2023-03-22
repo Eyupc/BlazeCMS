876a1dee74761b7ba485ce2036d0005649a5e5a5
unknown <eyuptrabzonn@gmail.com>
2023-03-22 23:11:39 +0100

    Create HomeButtons.tsx

---
+++ src/Components/Home/parts/HomeButtons.tsx
@@ -0,0 +1,10 @@
+import { memo } from 'react';
+
+export const HomeButtons = memo(() => {
+  return (
+    <div className="hotel-button">
+      <div className="enterhotel">OTELE GİR</div>
+      <div className="razewiki">DISCORD</div>
+    </div>
+  );
+});
