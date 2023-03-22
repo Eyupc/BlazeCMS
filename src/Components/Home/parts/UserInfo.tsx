d8f59c99523ca9eba3aa959d91c847dfbfc9b46f
unknown <eyuptrabzonn@gmail.com>
2023-03-22 23:11:44 +0100

    Create UserInfo.tsx

---
+++ src/Components/Home/parts/UserInfo.tsx
@@ -0,0 +1,28 @@
+import { memo } from 'react';
+import { IUserInfo } from '../interfaces/IUserInfo';
+
+export const UserInfo = memo(
+  ({ username, motto, last_online, achievement_score }: IUserInfo) => {
+    return (
+      <div className="user-info">
+        <div className="username">{username}</div>
+        <div className="motto">
+          <i className="fa fa-quote-left"></i>
+          <em>{motto}</em>
+          <i className="fa fa-quote-right"></i>
+        </div>
+        <div className="user-stats">
+          <strong>Son giriş: </strong> <>{last_online}</>
+        </div>
+        {
+          ///<div className="user-stats bank">
+          //<strong>Etkinlik puanı:</strong> 2
+          // </div>
+        }
+        <div className="user-stats level">
+          <strong>Başarı puanı:</strong> {achievement_score}
+        </div>
+      </div>
+    );
+  }
+);
