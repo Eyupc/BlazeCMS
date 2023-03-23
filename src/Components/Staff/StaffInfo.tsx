270e88accfc6cb718bdc543a69836cc3a26d3fbc
unknown <eyuptrabzonn@gmail.com>
2023-03-24 00:32:48 +0100

    Create StaffInfo.tsx

---
+++ src/Components/Staff/StaffInfo.tsx
@@ -0,0 +1,19 @@
+import { memo } from 'react';
+import { IStaffInfo } from './interfaces/IStaffInfo';
+export const StaffInfo = memo(({ rankName }: IStaffInfo) => {
+  return (
+    <div className="staff-info">
+      <div className="info-staff d-flex">
+        <div className="staffico"></div>
+        <div className="staff-text">
+          <div className="staff-tittle">{rankName}</div>
+          <div className="staff-description">
+            Raze staffs are people that trying to keep the hotel safe, friendly,
+            fun. These people are here to support you, if u got some problems
+            you can directly go to a staff for sue any problem or anything.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
