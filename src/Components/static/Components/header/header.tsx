745c84072554e2ea385ec4195133e7c66961c372
unknown <eyuptrabzonn@gmail.com>
2023-03-24 00:32:56 +0100

    Create header.tsx

---
+++ src/Components/static/Components/header/header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import { memo } from 'react';
+
+export const Header = memo(() => {
+  return (
+    <header>
+      <div className="landspace"></div>
+      <div className="landspace"></div>
+      <div className="landspace"></div>
+      <div className="clouds"></div>
+      <Link
+        href="/"
+        className="rplogo animate__animated animate__jackInTheBox"
+      ></Link>
+    </header>
+  );
+});
