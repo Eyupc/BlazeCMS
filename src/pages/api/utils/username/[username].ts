722d4d28fe2fa63d4606ad61ed8628530fb0cb5b
unknown <eyuptrabzonn@gmail.com>
2023-03-24 00:34:10 +0100

    Update [username].ts

--- src/pages/api/utils/username/[username].ts
+++ src/pages/api/utils/username/[username].ts
@@ -1,23 +1,21 @@
 import DatabaseManager from 'database/DatabaseManager';
-import { setServers } from 'dns';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getSession, useSession } from 'next-auth/react';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{}>
 ) {
   const username: string = req.query.username!.toString() || '';
-  let data = await DatabaseManager.GetInstance().UserQueries.UsernameExist(
+  const data = await DatabaseManager.GetInstance().UserQueries.UsernameExist(
     username
   );
   if (data) {
     res.status(200).json({
       exist: true
     });
-  } else {
-    res.status(200).json({
-      exist: false
-    });
+    return;
   }
+  res.status(200).json({
+    exist: false
+  });
 }
