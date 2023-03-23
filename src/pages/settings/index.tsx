28af6bf2ca0ffe88a29d13c0c157f3e65b90535d
unknown <eyuptrabzonn@gmail.com>
2023-03-24 00:34:30 +0100

    Settings page works!

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,40 @@
+import '@/app/global.css';
+import { AnnouncementBar } from '@/Components/Index/AnnouncementBar';
+import { AlertBox } from '@/Components/Register/AlertBox';
+import { SettingsBox } from '@/Components/Settings/SettingsBox';
+import { Header } from '@/Components/static/Components/header/header';
+import { Main } from '@/Components/static/Components/Main/main';
+import Navigator from '@/Components/static/Components/nav/navigator';
+import Head from 'next/head';
+import { useState } from 'react';
+
+export default function SettingsPage() {
+  const [credentials, setCredentials] = useState('');
+  return (
+    <>
+      <Head>
+        <title>Blaze - Settings</title>
+      </Head>
+      <Navigator loggedIn={true} />
+      <Header />
+      <AnnouncementBar
+        title={'Blaze settings page'}
+        description={
+          'Welcome back, this is the settings page of our hotel. Here, u can change some private information of yourself!'
+        }
+      />
+      <Main
+        child={
+          <div className="register">
+            <SettingsBox />
+            <AlertBox
+              title={'Blaze - Settings'}
+              description={'Change your user settings'}
+              text={''}
+            />
+          </div>
+        }
+      />
+    </>
+  );
+}
