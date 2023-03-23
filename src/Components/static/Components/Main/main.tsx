25c4cb1b859e23dab1bf83ab073fffb6e0fb7910
unknown <eyuptrabzonn@gmail.com>
2023-03-24 00:33:01 +0100

    Create main.tsx

---
+++ src/Components/static/Components/Main/main.tsx
@@ -0,0 +1,10 @@
+import '@/app/global.css';
+import { memo } from 'react';
+import { IMain } from './IMain';
+export const Main = memo(({ child }: IMain) => {
+  return (
+    <div className="content mt-2 mb-2">
+      <div className="section">{child}</div>
+    </div>
+  );
+});
