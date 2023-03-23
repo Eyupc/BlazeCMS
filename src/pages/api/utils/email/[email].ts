0d4441d348477fabaca9da9fea0e4e5aa9194fcf
unknown <eyuptrabzonn@gmail.com>
2023-03-24 00:34:08 +0100

    Update [email].ts

--- src/pages/api/utils/email/[email].ts
+++ src/pages/api/utils/email/[email].ts
@@ -1,21 +1,19 @@
 import DatabaseManager from 'database/DatabaseManager';
-import { setServers } from 'dns';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getSession, useSession } from 'next-auth/react';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{}>
 ) {
   const email: string = req.query.email!.toString() || '';
-  let data = await DatabaseManager.GetInstance().UserQueries.MailExist(email);
+  const data = await DatabaseManager.GetInstance().UserQueries.MailExist(email);
   if (data) {
     res.status(200).json({
       exist: true
     });
-  } else {
-    res.status(200).json({
-      exist: false
-    });
+    return;
   }
+  res.status(200).json({
+    exist: false
+  });
 }
