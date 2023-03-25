4bcebceaaa280547298a095eb2b9f936417a130c
unknown <eyuptrabzonn@gmail.com>
2023-03-25 01:14:49 +0100

    Update navigator.tsx

--- src/Components/static/Components/nav/navigator.tsx
+++ src/Components/static/Components/nav/navigator.tsx
@@ -1,20 +1,39 @@
 import { ShowLoginForm } from '@/Components/Index/methods/ShowLoginForm';
+import axios from 'axios';
 import { signOut } from 'next-auth/react';
 import Link from 'next/link';
 import router from 'next/router';
-import { useState } from 'react';
-import { INavigator } from './INavigator';
-export default function Navigator({ loggedIn }: INavigator) {
+import { useEffect, useState } from 'react';
+export default function Navigator() {
   const [showSub, setShowSub] = useState('n');
   const [showCommunity, setShowCommunity] = useState('n');
   const [showHome, setShowHome] = useState('n');
+  const [user, setUser] = useState<{
+    avatar: string;
+    username: string;
+  } | null>(null);
+
+  useEffect(() => {
+    new Promise(async (res, rej) => {
+      await axios('/api/user/session', {
+        method: 'GET'
+      })
+        .then((resp) => {
+          if (resp.data.status as Boolean)
+            setUser({ username: resp.data.username, avatar: resp.data.avatar });
+        })
+        .catch((err) => {
+          setUser(null);
+        });
+    });
+  }, []);
 
   return (
     <>
       <nav>
         <div className="content">
           <ul className="nav">
-            {loggedIn ? (
+            {user != null ? (
               <>
                 <li
                   className="home"
@@ -28,7 +47,6 @@ export default function Navigator({ loggedIn }: INavigator) {
                     setShowHome('n');
                   }}
                 >
-                  <Link href={'/home'} />
                   <div className="name">Home</div>
                 </li>
                 <li
@@ -48,18 +66,6 @@ export default function Navigator({ loggedIn }: INavigator) {
                 <li className="community" menuname="test">
                   <div className="name">Yardım</div>
                 </li>
-                <li className="logout">
-                  <Link
-                    href={'#'}
-                    onClick={async () => {
-                      await signOut({
-                        redirect: false
-                      });
-                      router.push('/');
-                    }}
-                  />
-                  <div className="name">Logout</div>
-                </li>
               </>
             ) : (
               <>
@@ -77,60 +83,64 @@ export default function Navigator({ loggedIn }: INavigator) {
         </div>
       </nav>
 
-      <div id="subnav" active={showSub}>
-        <div
-          className="subItem communityMenu"
-          onMouseOver={() => setShowSub('y')}
-          onMouseLeave={() => setShowSub('n')}
-          id="communityMenu"
-          active={showCommunity}
-        >
-          <div className="submenu-main">
-            <Link href={'/staff'} className="submenu-href">
-              <div className="submenu-box">
-                <div
-                  className="submenu-ico"
-                  style={{
-                    backgroundImage: 'url(/assets/images/staff.gif)'
-                  }}
-                ></div>
-                <div className="submenu-info">
-                  <div className="submenu-name">Blaze Staff</div>
-                  <div className="submenu-description">Blaze Personelleri</div>
-                </div>
-              </div>
-            </Link>
-            <a href="citymap.php" className="submenu-href">
-              <div className="submenu-box">
-                <div
-                  className="submenu-ico"
-                  style={{ backgroundImage: 'url(/assets/images/map.gif)' }}
-                ></div>
-                <div className="submenu-info">
-                  <div className="submenu-name">City Map</div>
-                  <div className="submenu-description">
-                    Check out the detailed map of the city
+      {user != null ? (
+        <div id="subnav" active={showSub}>
+          <div
+            className="subItem communityMenu"
+            onMouseOver={() => setShowSub('y')}
+            onMouseLeave={() => setShowSub('n')}
+            id="communityMenu"
+            active={showCommunity}
+          >
+            <div className="submenu-main">
+              <Link href={'/staff'} className="submenu-href">
+                <div className="submenu-box">
+                  <div
+                    className="submenu-ico"
+                    style={{
+                      backgroundImage: 'url(/assets/images/staff.gif)'
+                    }}
+                  ></div>
+                  <div className="submenu-info">
+                    <div className="submenu-name">Blaze Staff</div>
+                    <div className="submenu-description">
+                      Blaze Personelleri
+                    </div>
                   </div>
                 </div>
-              </div>
-            </a>
-            <a href="leaderboards.php" className="submenu-href">
-              <div className="submenu-box">
-                <div
-                  className="submenu-ico"
-                  style={{
-                    backgroundImage: 'url(/assets/images/leader.gif)'
-                  }}
-                ></div>
-                <div className="submenu-info">
-                  <div className="submenu-name">Leaderboards</div>
-                  <div className="submenu-description">
-                    Check the leaderboard list
+              </Link>
+              <Link href={'leaderboards'} className="submenu-href">
+                <div className="submenu-box">
+                  <div
+                    className="submenu-ico"
+                    style={{
+                      backgroundImage: 'url(/assets/images/leader.gif)'
+                    }}
+                  ></div>
+                  <div className="submenu-info">
+                    <div className="submenu-name">Leaderboards</div>
+                    <div className="submenu-description">
+                      Check the leaderboard list
+                    </div>
                   </div>
                 </div>
-              </div>
-            </a>
-            <a href="civilians-online.php" className="submenu-href">
+              </Link>
+              <a href="citymap.php" className="submenu-href">
+                <div className="submenu-box">
+                  <div
+                    className="submenu-ico"
+                    style={{ backgroundImage: 'url(/assets/images/map.gif)' }}
+                  ></div>
+                  <div className="submenu-info">
+                    <div className="submenu-name">City Map</div>
+                    <div className="submenu-description">
+                      Check out the detailed map of the city
+                    </div>
+                  </div>
+                </div>
+              </a>
+
+              {/*  <a href="civilians-online.php" className="submenu-href">
               <div className="submenu-box">
                 <div
                   className="submenu-ico"
@@ -141,34 +151,77 @@ export default function Navigator({ loggedIn }: INavigator) {
                   <div className="submenu-description">Active users list</div>
                 </div>
               </div>
-            </a>
+                </a>*/}
+            </div>
           </div>
-        </div>
-        <div
-          className="subItem homeMenu"
-          onMouseOver={() => setShowSub('y')}
-          onMouseLeave={() => setShowSub('n')}
-          id="communityMenu"
-          active={showHome}
-        >
-          <div className="submenu-main">
-            <Link href={'/settings'} className="submenu-href">
-              <div className="submenu-box">
-                <div
-                  className="submenu-ico"
-                  style={{
-                    backgroundImage: 'url(/assets/images/settings_icon.png)'
-                  }}
-                ></div>
-                <div className="submenu-info">
-                  <div className="submenu-name">Settings</div>
-                  <div className="submenu-description">User settings</div>
+          <div
+            className="subItem homeMenu"
+            onMouseOver={() => setShowSub('y')}
+            onMouseLeave={() => setShowSub('n')}
+            id="communityMenu"
+            active={showHome}
+          >
+            <div className="submenu-main">
+              <Link href={'/home'} className="submenu-href">
+                <div className="submenu-box">
+                  <div
+                    className="submenu-ico"
+                    style={{
+                      backgroundImage: `url(https://www.leet.ws/leet-imaging/avatarimage?figure=${
+                        user!.avatar
+                      }&head_direction=2&direction=2&size=sml)`,
+                      backgroundPositionY: '-35px'
+                    }}
+                  ></div>
+                  <div className="submenu-info">
+                    <div className="submenu-name">{user!.username}</div>
+                    <div className="submenu-description">Home page</div>
+                  </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+              <Link href={'/settings'} className="submenu-href">
+                <div className="submenu-box">
+                  <div
+                    className="submenu-ico"
+                    style={{
+                      backgroundImage: 'url(/assets/images/settings_icon.png)'
+                    }}
+                  ></div>
+                  <div className="submenu-info">
+                    <div className="submenu-name">Settings</div>
+                    <div className="submenu-description">User settings</div>
+                  </div>
+                </div>
+              </Link>
+              <Link
+                href={'#'}
+                className="submenu-href"
+                onClick={async () => {
+                  await signOut({
+                    redirect: false
+                  });
+                  router.push('/');
+                }}
+              >
+                <div className="submenu-box">
+                  <div
+                    className="submenu-ico"
+                    style={{
+                      backgroundImage: 'url(/assets/images/logout.gif)'
+                    }}
+                  ></div>
+                  <div className="submenu-info">
+                    <div className="submenu-name">Logout</div>
+                    <div className="submenu-description">End session</div>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
