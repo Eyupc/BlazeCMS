1ae65f88d1f7b88bd11d50a71fb6e7cae44e80d6
unknown <eyuptrabzonn@gmail.com>
2023-03-25 01:15:03 +0100

    Create session.ts

---
+++ src/pages/api/user/session.ts
@@ -0,0 +1,28 @@
+import DatabaseManager from 'database/DatabaseManager';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{}>
+) {
+  const session = await getSession({ req: req });
+  if (!session) {
+    res.status(404).json({ status: false });
+    return;
+  }
+  const data = await DatabaseManager.GetInstance().Query(
+    `SELECT id,username,look FROM users WHERE id = '${session.user.id}' LIMIT 1`
+  );
+  if (!data.error) {
+    res.status(200).json({
+      status: true,
+      username: data.data[0].username,
+      avatar: data.data[0].look
+    });
+    return;
+  }
+  res.status(200).json({
+    status: false
+  });
+}
