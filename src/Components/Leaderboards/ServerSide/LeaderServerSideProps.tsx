d451fe398381c80cf83381a8f98a78eda174c500
unknown <eyuptrabzonn@gmail.com>
2023-03-25 01:14:28 +0100

    Create LeaderServerSideProps.tsx

---
+++ src/Components/Leaderboards/ServerSide/LeaderServerSideProps.tsx
@@ -0,0 +1,30 @@
+import DatabaseManager from 'database/DatabaseManager';
+import { GetServerSidePropsContext } from 'next';
+import { ILeaderboardsComponent } from '../interfaces/ILeaderboardsComponent';
+
+export async function getServerSideProps(
+  ctx: GetServerSidePropsContext
+): Promise<{ props: ILeaderboardsComponent }> {
+  const credits = await DatabaseManager.GetInstance().UserLists.getTopList(
+    'credits'
+  );
+  const duckets = await DatabaseManager.GetInstance().UserLists.getTopList(
+    'duckets'
+  );
+  const diamonds = await DatabaseManager.GetInstance().UserLists.getTopList(
+    'diamonds'
+  );
+  const achievement_score =
+    await DatabaseManager.GetInstance().UserLists.getTopList(
+      'achievement_score'
+    );
+
+  return {
+    props: {
+      cr_topList: JSON.parse(JSON.stringify(credits.users)),
+      di_topList: JSON.parse(JSON.stringify(diamonds.users)),
+      du_topList: JSON.parse(JSON.stringify(duckets.users)),
+      as_topList: JSON.parse(JSON.stringify(achievement_score.users))
+    }
+  };
+}
