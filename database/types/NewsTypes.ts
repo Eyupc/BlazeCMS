93e9a41087371ed7373a3ef44fe402ebee5dec2e
unknown <eyuptrabzonn@gmail.com>
2023-04-03 04:13:14 +0200

    Create NewsTypes.ts

---
+++ database/types/NewsTypes.ts
@@ -0,0 +1,20 @@
+import { StatusType } from './StatusType';
+
+export type INews = {
+  status: StatusType['status'];
+  news?: NewsType[];
+};
+
+export type NewsType = {
+  id: number;
+  author_id: number;
+  title: string;
+  description: string;
+  full_story: string;
+  image: string;
+  timestamp: EpochTimeStamp;
+  enabled: boolean;
+
+  username: string;
+  avatar: string;
+};
