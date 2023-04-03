c4f34b53909c9e35ed22f00fd819aa98912c2f78
unknown <eyuptrabzonn@gmail.com>
2023-04-04 01:15:52 +0200

    getNews, getLatestNews by limit

--- database/News/NewsQueries.ts
+++ database/News/NewsQueries.ts
@@ -1,4 +1,4 @@
-import { INews, NewsType } from 'database/types/NewsTypes';
+import { ILatestNews, INews } from 'database/types/NewsTypes';
 import { Connection } from 'mysql';
 
 export class NewsQueries {
@@ -7,18 +7,32 @@ export class NewsQueries {
     this.connection = connection;
   }
 
-  public async getLatestNews(limit: number): Promise<INews> {
+  public async getNews(id: number): Promise<INews> {
     return await new Promise((res, rej) => {
       this.connection.query(
-        'SELECT c.*,u.username,u.look as avatar FROM `cms_news` c INNER JOIN `users` u ON c.`author_id`= u.`id` ORDER BY c.`id` DESC LIMIT ' +
-          limit,
-        (_err, results) => {
-          if (_err || results.length < 0) res({ status: false });
-
-          const news = results as Array<NewsType>;
+        'SELECT c.*,u.username,u.look as avatar FROM `cms_news` c INNER JOIN `users` u ON c.`author_id`= u.`id` WHERE c.`enabled`=1 AND c.`id` =' +
+          id,
+        (_error, results) => {
+          if (_error || results.length == 0) res({ status: false });
           res({
             status: true,
-            news: news
+            news: results[0]
+          });
+        }
+      );
+    });
+  }
+  public async getLatestNews(limit: number): Promise<ILatestNews> {
+    return await new Promise((res, rej) => {
+      this.connection.query(
+        'SELECT c.*,u.username,u.look as avatar FROM `cms_news` c INNER JOIN `users` u ON c.`author_id`= u.`id` WHERE c.`enabled` = 1 ORDER BY c.`id` DESC LIMIT ' +
+          limit,
+        (_err, results) => {
+          if (_err || results.length == 0) res({ status: false });
+
+          res({
+            status: true,
+            news: results
           });
         }
       );
