20ffca28e82ce7a3374495eed5d98d810452358f
unknown <eyuptrabzonn@gmail.com>
2023-04-11 19:49:05 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,46 +8,52 @@ import Header from '@/Components/static/Components/header/header';
 import Main from '@/Components/static/Components/Main/main';
 import Navigator from '@/Components/static/Components/nav/navigator';
 import cnf from 'cms-config.json';
+import { ReCaptchaProvider } from 'next-recaptcha-v3';
 import Head from 'next/head';
 import '/styles/styles.css';
 export default function IndexPage({ csrfToken }: IIndexPage) {
   return (
     <>
-      <Head>
-        <title>{cnf.texts.INDEX_TITLE}</title>
-      </Head>
-      <Navigator />
-      <Header />
-      <AnnouncementBar
-        title={cnf.texts.INDEX_AB_TITLE}
-        description={cnf.texts.INDEX_AB_DESC}
-      />
-      <Main
-        child={
-          <div className="login">
-            <InformationBox
-              alertTitle={cnf.texts.INDEX_IB1_ALERT}
-              title={cnf.texts.INDEX_IB1_TITLE}
-              description={cnf.texts.INDEX_IB1_DESC}
-              banner_img={cnf.texts.INDEX_IB1_IMG}
-            />
-            <InformationBox
-              alertTitle={cnf.texts.INDEX_IB2_ALERT}
-              title={cnf.texts.INDEX_IB2_TITLE}
-              description={cnf.texts.INDEX_IB2_DESC}
-              banner_img={cnf.texts.INDEX_IB2_IMG}
-            />
-            <InformationBox
-              alertTitle={cnf.texts.INDEX_IB3_ALERT}
-              title={cnf.texts.INDEX_IB3_TITLE}
-              description={cnf.texts.INDEX_IB3_DESC}
-              banner_img={cnf.texts.INDEX_IB3_IMG}
-            />
-          </div>
-        }
-      />
-      <ModalBox />
-      <Footer />
+      <ReCaptchaProvider
+        reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+      >
+        {' '}
+        <Head>
+          <title>{cnf.texts.INDEX_TITLE}</title>
+        </Head>
+        <Navigator />
+        <Header />
+        <AnnouncementBar
+          title={cnf.texts.INDEX_AB_TITLE}
+          description={cnf.texts.INDEX_AB_DESC}
+        />
+        <Main
+          child={
+            <div className="login">
+              <InformationBox
+                alertTitle={cnf.texts.INDEX_IB1_ALERT}
+                title={cnf.texts.INDEX_IB1_TITLE}
+                description={cnf.texts.INDEX_IB1_DESC}
+                banner_img={cnf.texts.INDEX_IB1_IMG}
+              />
+              <InformationBox
+                alertTitle={cnf.texts.INDEX_IB2_ALERT}
+                title={cnf.texts.INDEX_IB2_TITLE}
+                description={cnf.texts.INDEX_IB2_DESC}
+                banner_img={cnf.texts.INDEX_IB2_IMG}
+              />
+              <InformationBox
+                alertTitle={cnf.texts.INDEX_IB3_ALERT}
+                title={cnf.texts.INDEX_IB3_TITLE}
+                description={cnf.texts.INDEX_IB3_DESC}
+                banner_img={cnf.texts.INDEX_IB3_IMG}
+              />
+            </div>
+          }
+        />
+        <ModalBox />
+        <Footer />
+      </ReCaptchaProvider>
     </>
   );
 }
