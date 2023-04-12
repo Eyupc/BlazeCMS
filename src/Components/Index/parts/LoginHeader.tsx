6c6f11074c6496c9ef72174fe67f08570f3f0fe2
unknown <eyuptrabzonn@gmail.com>
2023-04-12 04:41:04 +0200

    Update LoginHeader.tsx

--- src/Components/Index/parts/LoginHeader.tsx
+++ src/Components/Index/parts/LoginHeader.tsx
@@ -6,10 +6,8 @@ export function LoginHeader({ title, description }: ILoginHeader) {
     <div className="info-login d-flex">
       <div className="pins"></div>
       <div className="login-text">
-        <div className="login-title">BLAZE'YE GİRİŞ YAP</div>
-        <div className="login-description">
-          Merhaba, sizleri özledik.. Bizimle oynamak için giriş yapın!
-        </div>
+        <div className="login-title">{title}</div>
+        <div className="login-description">{description}</div>
       </div>
       <div className="modalclose" onClick={() => ShowLoginForm(false)}>
         x
